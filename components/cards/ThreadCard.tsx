b58300bc55c98b1975f66c0e48aeedf7db59f052
Ayushnikov <ayushnikovv@gmail.com>
2025-01-02 20:48:45 +0530

    implement all featurs

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -113,32 +113,10 @@ function ThreadCard({
             </div>
           </div>
         </div>
-
+              {/*todo delete*/ }
         
         
-      </div>
-
-      {!isComment && comments.length > 0 && (
-        <div className='ml-1 mt-3 flex items-center gap-2'>
-          {comments.slice(0, 2).map((comment, index) => (
-            <Image
-              key={index}
-              src={comment.author.image}
-              alt={`user_${index}`}
-              width={24}
-              height={24}
-              className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
-            />
-          ))}
-
-          <Link href={`/thread/${id}`}>
-            <p className='mt-1 text-subtle-medium text-gray-1'>
-              {comments.length} repl{comments.length > 1 ? "ies" : "y"}
-            </p>
-          </Link>
-        </div>
-      )}
-
+     
       {!isComment && community && (
         <Link
           href={`/communities/${community.id}`}
@@ -146,7 +124,7 @@ function ThreadCard({
         >
           <p className='text-subtle-medium text-gray-1'>
             {formatDateString(createdAt)}
-            {community && ` - ${community.name} Community`}
+            - ${community.name} Community
           </p>
 
           <Image
@@ -158,6 +136,7 @@ function ThreadCard({
           />
         </Link>
       )}
+       </div>
     </article>
   );
 }
--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -10,7 +10,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { ObjectId } from "mongoose";
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
-
+import { useOrganization } from "@clerk/nextjs";
 
 interface props{
     user:{
@@ -27,6 +27,7 @@ interface props{
 function PostThread({userId}: {userId:string}) {
     const router = useRouter();
     const pathname= usePathname();
+    const {organization}= useOrganization();
 
     const form = useForm({
         resolver:zodResolver(ThreadValidation),
@@ -37,10 +38,11 @@ function PostThread({userId}: {userId:string}) {
     })
 
     const onSubmit = async (values: z.infer<typeof ThreadValidation>) => {
-        await createThread({
+       
+      await createThread({
           text: values.thread,
           author: userId,
-          communityId: null,
+          communityId: organization ? organization.id: null,
           path: pathname,
         });
         router.push("/")
